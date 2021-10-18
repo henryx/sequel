6f3e112497c2b7da9882c6743191da81ceac3f2f
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-10-18 23:38:48 +0200

    Added test

--- src/test/java/qubu/QueryTest.java
+++ src/test/java/qubu/QueryTest.java
@@ -16,10 +16,20 @@ public class QueryTest {
     }
 
     @Test
-    public void testEmpty() {
+    public void testEmptyFrom() {
         String result = "";
         String q = Query.from("")
                 .select("t1", "t2")
+                .getSql();
+
+        Assert.assertEquals(q, result);
+    }
+
+    @Test
+    public void testEmptySelect() {
+        String result = "";
+        String q = Query.from("test")
+                .select("")
                 .getSql();
 
         Assert.assertEquals(q, result);
