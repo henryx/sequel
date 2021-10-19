7513e239fd38e9c253480042ab719200ac279c28
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-10-19 18:23:08 +0200

    Implemented equality filter in Criterion class

--- src/main/java/qubu/Criterion.java
+++ src/main/java/qubu/Criterion.java
@@ -4,4 +4,24 @@ package qubu;
  * Criterion class set criterion used for filtering data
  */
 public class Criterion {
+    private final String criterion;
+
+    private Criterion(String criterion) {
+        this.criterion = criterion;
+    }
+
+    private static String build(String... pieces) {
+        return String.join(" ", pieces);
+    }
+
+    /**
+     * Equality filter
+     *
+     * @param col1 Column at left of the filter
+     * @param col2 Column at the right of the filter
+     * @return a builder instance of the class
+     */
+    public static Criterion eq(String col1, String col2) {
+        return new Criterion(build(col1, "=", col2));
+    }
 }
