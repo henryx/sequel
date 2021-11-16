aa8b5036ad9598d52326e832aa9dc77c902f5606
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-11-16 17:22:49 +0100

    Added test

--- src/test/java/com/github/henryx/qubu/QueryTest.java
+++ src/test/java/com/github/henryx/qubu/QueryTest.java
@@ -16,6 +16,17 @@ public class QueryTest {
     }
 
     @Test
+    public void testFromLimit() {
+        String expected = "SELECT t1, t2 FROM test FETCH FIRST 10 ROWS ONLY";
+        String q = Query.from("test")
+                .select("t1", "t2")
+                .limit(10)
+                .getSql();
+
+        Assert.assertEquals(expected, q);
+    }
+
+    @Test
     public void testEmptyFrom() {
         String expected = "";
         String q = Query.from("")
